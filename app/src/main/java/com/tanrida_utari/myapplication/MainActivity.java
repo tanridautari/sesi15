bdf2ae8444b9c10857fb5f0e0de00a8d18016ac1
FahrisalFR <fahrisalfr@gmail.com>
2019-07-06 11:04:55 +0700

    sesi15

---
+++ app/src/main/java/com/tanrida_utari/myapplication/MainActivity.java
@@ -0,0 +1,71 @@
+package com.tanrida_utari.myapplication;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.tanrida_utari.myapplication.fragment.BlankFragment;
+import com.tanrida_utari.myapplication.fragment.BlankFragment2;
+
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener, BlankFragment2.OnFragmentInteractionListener{
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    addFragment(new BlankFragment(), true, "Satu");
+                    return true;
+                case R.id.navigation_dashboard:
+                    addFragment(new BlankFragment2(), true, "Dua");
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void addFragment(Fragment fragment, boolean addToBackStack, String tag) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if(addToBackStack) {
+            ft.addToBackStack(tag);
+        }
+
+        ft.replace(R.id.message, fragment, tag);
+        ft.commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
